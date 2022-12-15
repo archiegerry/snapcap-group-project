82dda02fc511c7decbaca1ed339602ae375d902f
James Dering <sc21jcd@leeds.ac.uk>
2022-12-15 20:59:26 +0000

    Added icons for fx, shrunk fx to 4x1

--- scrub.cpp
+++ scrub.cpp
@@ -103,31 +103,31 @@ void Scrub::createWidgets()
     audiobuttons.at(0).setText("crop");
     audiobuttons.at(1).setText("cut");
     audiobuttons.at(2).setText("splice");
-    audiobuttons.at(3).setText("smth");
-    audiobuttons.at(4).setText("eq");
-    audiobuttons.at(5).setText("audio option");
-    audiobuttons.at(6).setText("audio option");
-    audiobuttons.at(7).setText("audio option");
+    audiobuttons.at(3).setText("eq");
+//    audiobuttons.at(4).setText("audio option");
+//    audiobuttons.at(5).setText("audio option");
+//    audiobuttons.at(6).setText("audio option");
+//    audiobuttons.at(7).setText("audio option");
     videobuttons.at(0).setText("crop");
     videobuttons.at(1).setText("cut");
     videobuttons.at(2).setText("splice");
-    videobuttons.at(3).setText("smth");
-	videobuttons.at(4).setText("fx");
-	videobuttons.at(5).setText("video option");
-	videobuttons.at(6).setText("video option");
-	videobuttons.at(7).setText("video option");
+    videobuttons.at(3).setText("fx");
+//	videobuttons.at(4).setText("video option");
+//	videobuttons.at(5).setText("video option");
+//	videobuttons.at(6).setText("video option");
+//	videobuttons.at(7).setText("video option");
 
 
     //add options and effects
     for (int i=0;i<NO_OF_AUDIOOPTIONS;i++){
-        audiobuttons.at(i).setSizePolicy(QSizePolicy::Expanding,QSizePolicy::Expanding);
+        audiobuttons.at(i).setSizePolicy(QSizePolicy::Expanding,QSizePolicy::Minimum);
         audiobuttons.at(i).setStyleSheet("background-color: red");
         audiobuttonlayout->addWidget(&audiobuttons.at(i),i/4,i%4);
     }
     audiooptions->setLayout(audiobuttonlayout);
 
     for (int i=0;i<NO_OF_VIDEOOPTIONS;i++){
-        videobuttons.at(i).setSizePolicy(QSizePolicy::Expanding,QSizePolicy::Expanding);
+        videobuttons.at(i).setSizePolicy(QSizePolicy::Expanding,QSizePolicy::Minimum);
         videobuttons.at(i).setStyleSheet("background-color: blue");
         videobuttonlayout->addWidget(&videobuttons.at(i),i/4,i%4);
     }
