593f5effbf65e66f707fa77f79cb47e5f7e37c2b
Jack <derringoj@gmail.com>
2022-12-16 15:58:57 +0000

    Replaced load with export

--- headerButtons.cpp
+++ headerButtons.cpp
@@ -14,23 +14,23 @@ void headerButtons::createHeader()
     QHBoxLayout *header = new QHBoxLayout();
 
     QPushButton * newProject = new QPushButton("");
-    QPushButton * loadProject = new QPushButton("");
+    QPushButton * exportProject = new QPushButton("");
     settings = new QPushButton("");
 
     newProject -> setFixedSize(QSize(50,50));
     settings -> setFixedSize(QSize(50,50));
-    loadProject -> setFixedSize(QSize(50,50));
+    exportProject -> setFixedSize(QSize(50,50));
 
     newProject -> setIcon(QIcon(":/new.png"));
     newProject -> setIconSize(QSize(50,50));
     settings -> setIcon(QIcon(":/settings.png"));
     settings -> setIconSize(QSize(50,50));
-    loadProject -> setIcon(QIcon(":/load.png"));
-    loadProject -> setIconSize(QSize(50,50));
+    exportProject -> setIcon(QIcon(":/export.png"));
+    exportProject -> setIconSize(QSize(60,60));
 
     header ->addWidget(settings);
 	header ->addWidget(newProject);
-	header ->addWidget(loadProject);
+    header ->addWidget(exportProject);
 
     setLayout(header);
 
